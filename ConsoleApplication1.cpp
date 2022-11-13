ee4ebf34bcc8577a6379d3187ec089a0375c445e
1AinCraD1 <alexei109356@gmail.com>
2022-11-13 17:31:50 +0200

    Create ConsoleApplication1.cpp
    Змінені дані

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <iomanip>
+#include "math.h"
+using namespace std;
+int main()
+{
+    int a, c;
+    float h;
+    cout << "Enter a = ";
+    cin >> a;
+    cout << "Enter c = ";
+    cin >> c;
+    cout << "Enter h = ";
+    cin >> h;
+    float y, x = a;
+    while (x <= c)
+    {
+        y = std::log(x * x) + pow(x, 2) + 2;
+        cout << setw(10) << "x=" << x << setw(10) << "y=" << y << endl;
+        x += h;
+    }
+    system("pause");
+    return 0;
+}
